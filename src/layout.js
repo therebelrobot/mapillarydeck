8e28c9886c1020de7de3a03e53584dab9e50a73f
Trent Oswald <trentoswald@therebelrobot.com>
2016-02-13 16:11:22 -0800

    style(layout): finished layout. taking a break for coffee

--- src/layout.js
+++ src/layout.js
@@ -2,6 +2,49 @@ var m = require('mithril')
 
 module.exports = function(props){
   return function mithrilLayout(ctrl){
-    return m('h1', 'Hello')
+    return m('.wrapper', [
+      m('nav.mui-appbar',[
+        m('.logo',[
+          m('i.fa.fa-map-o'),
+          ' ',
+          'MapillaryDeck'
+        ])
+      ]),
+      m('section.content',[
+        m('article.column.mui-panel',[
+          m('.card.mui-panel',[
+            m('img.avatar', {src:'http://placehold.it/100x100'})
+          ]),
+          m('.card.mui-panel'),
+          m('.card.mui-panel'),
+          m('.card.mui-panel'),
+          m('.card.mui-panel'),
+        ]),
+
+          m('article.column.mui-panel',[
+            m('.card.mui-panel'),
+            m('.card.mui-panel'),
+            m('.card.mui-panel'),
+            m('.card.mui-panel'),
+            m('.card.mui-panel'),
+          ]),
+
+            m('article.column.mui-panel',[
+              m('.card.mui-panel'),
+              m('.card.mui-panel'),
+              m('.card.mui-panel'),
+              m('.card.mui-panel'),
+              m('.card.mui-panel'),
+            ]),
+
+              m('article.column.mui-panel',[
+                m('.card.mui-panel'),
+                m('.card.mui-panel'),
+                m('.card.mui-panel'),
+                m('.card.mui-panel'),
+                m('.card.mui-panel'),
+              ])
+      ])
+    ])
   }
 }
